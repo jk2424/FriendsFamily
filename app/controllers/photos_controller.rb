4944c69827eb2ea60de73e9c5d790c47d4bf7b78
Jorge Orta <jorta012@fiu.edu>
2018-04-23 09:55:02 -0400

    Design background

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,16 +1,7 @@
 class PhotosController < ApplicationController
-  before_action :set_photo, only: [:show, :edit, :update, :destroy]
-
-
-  def display_img
-    @img = Image.find(params[:id])
-    send_data(@img.img, :type => @img.filetype, :filename => @img.filename,
-              :disposition => 'inline')
-  end
 
 
   def index
-    @photos = Photo.all
     hash = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=Miami,us&units=imperial&appid=69ee1fd6d0d4bafa14b8b23945a92179").parsed_response
     @cur_temp = (hash["main"]["temp"]).to_i
     @cur_desc = hash["weather"][0]["main"]
