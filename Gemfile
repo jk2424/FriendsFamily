72fe427dbae2c037402fa9d8d62a6ab849eaa6cf
jk2424 <jkim2244@gmail.com>
2018-04-24 13:15:23 -0400

    merging

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
+gem 'rails', '~> 5.2.0'
+# Use sqlite3 as the database for Active Record
 
 gem 'bootstrap-modal-rails'
 
@@ -12,8 +14,7 @@ gem 'pry'
 gem 'httparty'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.2.0'
-# Use sqlite3 as the database for Active Record
+
 gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,8 +37,26 @@ $(document).on('turbolinks:load', function(){
 });
 
 
-$(document).on('click', '.location', function() {
 
-  $('.zipform').show();
-
+$(function() {
+//----- OPEN
+$('[data-popup-open]').on('click', function(e)  {
+var targeted_popup_class = jQuery(this).attr('data-popup-open');
+$('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+e.preventDefault();
 });
+//----- CLOSE
+$('[data-popup-close]').on('click', function(e)  {
+var targeted_popup_class = jQuery(this).attr('data-popup-close');
+$('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+e.preventDefault();
+});
+});
+
+
+
+// $(document).on('click', '.location', function() {
+//
+//   $('.zipform').show();
+//
+// });
